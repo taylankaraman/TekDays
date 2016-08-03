e7fc582c1eea83891771ae6d678cc925cc37efeb
Taylan Karaman <taylan.karaman@gmail.com>
2016-08-04 06:54:17 +1000

    Initial commit

---
+++ grails-app/domain/com/tekdays/TekEvent.groovy
@@ -0,0 +1,19 @@
+package com.tekdays
+
+class TekEvent {
+
+    String city
+    String name
+    String organizer
+    String venue
+    Date startDate
+    Date endDate
+    String description
+
+    String toString(){
+        "$name, $city"
+    }
+
+    static constraints = {
+    }
+}
---
+++ test/unit/com/tekdays/TekEventTests.groovy
@@ -0,0 +1,22 @@
+package com.tekdays
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(TekEvent)
+class TekEventTests {
+
+    void testToString() {
+        when: "a tekEvent has a name and a city"
+        def tekEvent = new TekEvent(name:'Groovy One',
+                city: 'San Francisco',
+                organizer: 'John Doe')
+        then: "the toString method will combine them."
+        tekEvent.toString() == 'Groovy One, San Francisco'
+    }
+}
